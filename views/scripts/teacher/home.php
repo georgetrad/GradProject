d0c726de023363c96abb076daa5ec98350e8ba4d
George Trad <georgy.trad@gmail.com>
2014-04-19 21:33:55 +0300

    My first commit.

--- views/scripts/teacher/home.php
+++ views/scripts/teacher/home.php
@@ -1,7 +1,7 @@
 <?php
 require '../../../models/core.php';
 if (loggedIn() && $_SESSION['userType'] == 'U'){
-    echo 'You are logged in as a dean';
+    echo 'You are logged in as a teacher';
 }
 else{
     echo 'You do not have access to this page.';
