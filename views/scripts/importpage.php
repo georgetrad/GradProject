8bb5dd3de3d68f16955a8ecc631cdd402a8b7491
Mohammad <Mohammad@Mohammad-PC>
2014-04-22 21:08:54 +0300

    Merge origin/master

---
+++ views/scripts/importpage.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once '../../models/db_connect.php';
+include_once '../../gen/PHPExcel/IOFactory.php';
+include_once '../../models/importFunction.php';
+    
+//*******************Student name*******************//
+//configurations 
+$inputFileName = '../../uploads/crn.xlsx';
+$columns = array(
+    "id"            => "A",
+    "first_name"    => "B",
+    "middle_name"   => "C",
+    "last_name"     => "D"    
+);
+$table_name = 'student';
+$rows = 5000;
+$rowsOffSet = 1;
+
+$a = import($inputFileName, $columns, $table_name, $rows, $rowsOffSet);
+var_dump($a);echo '<br>';
+
