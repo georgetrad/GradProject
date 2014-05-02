a6c60cb1f9d219162a5a6974cf78d1da18c672d8
Mohammad <Mohammad@Mohammad-PC>
2014-05-03 00:53:10 +0300

    Mohammad, upload file

---
+++ views/scripts/dean/import.php
@@ -0,0 +1,16 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/GradProject/models/core.php';
+$title = UPLOAD;
+include_once $_SERVER['DOCUMENT_ROOT'].'/GradProject/views/scripts/general/header.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/GradProject/views/scripts/dean/top_bar.php';
+
+$dir = $_SERVER['DOCUMENT_ROOT'].'/GradProject/uploads/';
+
+$files1 = scandir($dir);
+
+echo '<br><br>Import page:<br><br>';
+
+foreach ($files1 as $data){
+    if (!($data == '..'||$data == '.'))
+        echo '<a href="">Click to import: "'.$data.'"</a><br>';    
+}
