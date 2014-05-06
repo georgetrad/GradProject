8ff1586d88a63573bbb9092ef057fa571515ebd6
Mohammad <Mohammad@192.168.2.7>
2014-05-07 00:31:11 +0300

    Merge origin/master

--- views/scripts/dean/student_grade.php
+++ views/scripts/dean/student_grade.php
@@ -14,7 +14,21 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/GradProject/views/scripts/dean/top_bar.
         <div class="medium-2 large-2 columns show-for-medium-up">
            &nbsp;
         </div> 
+        <div class="medium-2 large-2 columns show-for-medium-up">            
+            <span>الرجاء إدخال رقم الطالب: </span>
+        </div> 
+        <div class="medium-8 large-8 columns show-for-medium-up">
+            &nbsp;
+        </div> 
+    </form>       
+</div>  
+<div class="row">   
+    <br>
+    <form>
         <div class="medium-2 large-2 columns show-for-medium-up">
+           &nbsp;
+        </div> 
+        <div class="medium-2 large-2 columns show-for-medium-up">            
             <input type="search" id="search_text" />
         </div> 
         <div class="medium-1 large-1 columns show-for-medium-up">
