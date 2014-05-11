b71794895ef864deff6c6aa4adc9bb371a6f588c
Mohammad <Mohammad@192.168.2.7>
2014-05-11 23:49:46 +0300

    Merge origin/master
    Conflicts:
    	views/js/custom/all_courses_script.php

--- models/functions/homeFunction.php
+++ models/functions/homeFunction.php
@@ -7,7 +7,7 @@ echo $num[0];
 
 echo '<br>';
 
-$num = select('id', 'course');
+$num = select('count(id)', 'course');
 echo 'عدد المقررات هو: ';
 echo $num[0];
 
@@ -19,9 +19,9 @@ echo $num[0];
 
 echo '<br>';
 
-$num = select('count(id)', 'semester');
+//$num = select('count(id)', 'semester');
 echo 'عدد  الطلاب الناجحين في مقرر للغة الإنكليزية  1   هو: ';
-echo $num[0];
+//echo $num[0];
 
 include_once $_SERVER['DOCUMENT_ROOT'].'/GradProject/models/db_connect.php';
 
@@ -40,5 +40,49 @@ $query ='   SELECT  count(duty.id)
 $result =  mysql_query($query);
     
 while($row = mysql_fetch_array($result)) {
-    var_dump( $row );
+    echo $row[0];
+}
+echo '<br>';
+
+//$num = select('count(id)', 'semester');
+echo 'الطالب  10810001 أنجز عدد مواد  يساوي :';
+//echo $num[0];
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/GradProject/models/db_connect.php';
+
+$query ='   SELECT  count(duty.id)
+            FROM    student
+                INNER JOIN student_class ON student.id = student_class.student_id
+                    AND student.id = "10810001"
+                INNER JOIN class ON student_class.class_id = class.id
+                INNER JOIN course ON class.course_id = course.id   
+                INNER JOIN duty ON duty.student_class_student_id = student_class.student_id
+                    AND duty.student_class_class_id = student_class.class_id     
+                    AND duty.grade >=50 
+                INNER JOIN duty_type ON duty_type.id = duty.duty_type_id
+                    AND duty.duty_type_id = "5"  ';
+
+$result =  mysql_query($query);
+    
+while($row = mysql_fetch_array($result)) {
+    echo $row[0];
+}
+echo '<br>';
+
+//$num = select('count(id)', 'semester');
+echo 'الطالب  10810001 سجل  عدد شعب  يساوي :';
+//echo $num[0];
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/GradProject/models/db_connect.php';
+
+$query ='   SELECT  count(class.id)
+            FROM    student
+                INNER JOIN student_class ON student.id = student_class.student_id
+                    AND student.id = "10810001"
+                INNER JOIN class ON student_class.class_id = class.id ';
+
+$result =  mysql_query($query);
+    
+while($row = mysql_fetch_array($result)) {
+    echo $row[0];
 }
